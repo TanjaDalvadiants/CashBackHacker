77831a4fc0042223b926cc12b7c08bce28c920e7
tatiana dalvadiants <eparis@atlassian.com>
2023-04-21 15:14:32 +0400

    build failed

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -32,7 +32,7 @@ class CashbackHackServiceTest {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
         int actual = cashbackHackService.remain(amount);
-        int expected  = 1000;
+        int expected  = 0;
         Assertions.assertEquals(expected, actual);
 
     }
