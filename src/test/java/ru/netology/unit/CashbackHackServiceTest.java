b98083fd0dce74931c66133ad88750927da28715
tatiana dalvadiants <eparis@atlassian.com>
2023-04-22 00:05:40 +0400

    junit4-platform added

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -32,7 +32,7 @@ public  class CashbackHackServiceTest {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
         int actual = cashbackHackService.remain(amount);
-        int expected  = 0;
+        int expected  = 1000;
         Assert.assertEquals(expected, actual);
 
     }
