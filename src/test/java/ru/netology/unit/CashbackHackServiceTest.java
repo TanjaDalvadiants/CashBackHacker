9affbb16284991108f971eef535355adab2a1e43
tatiana dalvadiants <eparis@atlassian.com>
2023-04-26 16:16:25 +0400

    all added

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,39 +1,66 @@
 package ru.netology.unit;
 
-import org.junit.Assert;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 
 
 public  class CashbackHackServiceTest {
-    @Test
+    @org.junit.Test
+    public void shouldFindCashbackIfAmountLessThanThousand() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1;
+        int actual = service.remain(999);
+
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void shouldFindCashbackIfAmountEqualsThousand() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
     public void shouldFindCashbackIfAmountMoreThanThousand() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 999;
+        int actual = service.remain(1001);
+
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldFindCashbackIfAmountMoreThanThousandSecond() {
 
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1001;
         int actual = cashbackHackService.remain(amount);
         int expected  = 999;
-        Assert.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
 
     }
     @Test
-    public void shouldFindCashbackIfAmountLessThanThousand() {
+    public void shouldFindCashbackIfAmountLessThanThousandSecond() {
 
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 999;
         int actual = cashbackHackService.remain(amount);
         int expected  = 1;
-        Assert.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
 
     }
     @Test
-    public void shouldFindCashbackIfAmountEqualsThousand() {
+    public void shouldFindCashbackIfAmountEqualsThousandSecond() {
 
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
         int actual = cashbackHackService.remain(amount);
         int expected  = 0;
-        Assert.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
 
     }
 
